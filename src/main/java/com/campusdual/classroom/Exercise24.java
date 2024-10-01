34736deaa96f749edfa2b8f72d6d9b8ac219be58
Juan Fuente <127140423+Juan-Fuente-T@users.noreply.github.com>
2024-10-01 11:47:36 +0200

    Ejercicio 24 resuelto

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -5,14 +5,25 @@ import java.util.Queue;
 
 public class Exercise24 {
     public static Queue<String> createQueue() {
+        String[] elements = new String[]{"Smith", "Montessori", "Peralta", "House"};
+        Queue<String> newQueue = new LinkedList<>();
 
+        for (int i = 0; i < elements.length; i++) {
+            newQueue.offer(elements[i]);
+        }
+        return newQueue;
     }
-    public static void printAndEmptyQueue(Queue<String> queue) {
 
+    public static void printAndEmptyQueue(Queue<String> queue) {
+        int size = queue.size();
+        for (int i = 0; i < size; i++) {
+            System.out.println(queue.poll());
+        }
     }
 
     public static void main(String[] args) {
-
+        Queue<String> newQueue = createQueue();
+        printAndEmptyQueue(newQueue);
     }
 
 }
